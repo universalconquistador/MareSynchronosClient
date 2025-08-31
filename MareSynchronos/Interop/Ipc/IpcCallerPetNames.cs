55bd8d6f21ac8bb469f59313121462a0b220b261
Suterusu Kusanagi <suterusu@suterusu.com>
2025-08-30 20:32:46 -0400

    Final name fix?

--- MareSynchronos/Interop/Ipc/IpcCallerPetNames.cs
+++ MareSynchronos/Interop/Ipc/IpcCallerPetNames.cs
@@ -32,7 +32,7 @@ public sealed class IpcCallerPetNames : IIpcCaller
 
         _petnamesReady = pi.GetIpcSubscriber<object>("PetRenamer.OnReady");
         _petnamesDisposing = pi.GetIpcSubscriber<object>("PetRenamer.OnDisposing");
-        _apiVersion = pi.GetIpcSubscriber<(uint, uint)>("PetRenamer.GetApiVersion");
+        _apiVersion = pi.GetIpcSubscriber<(uint, uint)>("PetRenamer.ApiVersion");
         _enabled = pi.GetIpcSubscriber<bool>("PetRenamer.IsEnabled");
 
         _playerDataChanged = pi.GetIpcSubscriber<string, object>("PetRenamer.OnPlayerDataChanged");
