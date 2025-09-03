0ff3e17cbfd9141f364ffa070c9d6b7d89f37819
UniversalConquistador <universalconquistador@gmail.com>
2025-09-03 15:00:38 -0700

    Initial NSFW warning support.

--- MareSynchronos/WebAPI/SignalR/ApiController.Functions.Groups.cs
+++ MareSynchronos/WebAPI/SignalR/ApiController.Functions.Groups.cs
@@ -45,10 +45,10 @@ public partial class ApiController
         await _mareHub!.SendAsync(nameof(GroupClear), group).ConfigureAwait(false);
     }
 
-    public async Task<GroupJoinDto> GroupCreate()
+    public async Task<GroupJoinDto> GroupCreate(bool showNsfwWarning)
     {
         CheckConnection();
-        return await _mareHub!.InvokeAsync<GroupJoinDto>(nameof(GroupCreate)).ConfigureAwait(false);
+        return await _mareHub!.InvokeAsync<GroupJoinDto>(nameof(GroupCreate), showNsfwWarning).ConfigureAwait(false);
     }
 
     public async Task<List<string>> GroupCreateTempInvite(GroupDto group, int amount)
