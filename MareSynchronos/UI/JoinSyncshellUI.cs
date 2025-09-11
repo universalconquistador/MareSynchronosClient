626e0c67db6588a6fe2305d1befb18ba3ca8614f
UniversalConquistador <universalconquistador@gmail.com>
2025-09-10 22:31:32 -0700

    Fixes joining passwordless shell

--- MareSynchronos/UI/JoinSyncshellUI.cs
+++ MareSynchronos/UI/JoinSyncshellUI.cs
@@ -77,7 +77,7 @@ internal class JoinSyncshellUI : WindowMediatorSubscriberBase
             ImGui.TextUnformatted("Syncshell Password");
             ImGui.SameLine(200);
             ImGui.InputTextWithHint("##syncshellpw", "Password", ref _syncshellPassword, 50, ImGuiInputTextFlags.Password);
-            using (ImRaii.Disabled(string.IsNullOrEmpty(_desiredSyncshellToJoin) || string.IsNullOrEmpty(_syncshellPassword)))
+            using (ImRaii.Disabled(string.IsNullOrEmpty(_desiredSyncshellToJoin)))
             {
                 if (_uiSharedService.IconTextButton(Dalamud.Interface.FontAwesomeIcon.Plus, "Join Syncshell"))
                 {
