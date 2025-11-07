794f135b1e3ad865b04a92e2e696461ead3de320
Suterusu Kusanagi <suterusu@suterusu.com>
2025-11-07 15:31:06 -0500

    Update config defaults

--- PlayerSync/MareConfiguration/Configurations/MareConfig.cs
+++ PlayerSync/MareConfiguration/Configurations/MareConfig.cs
@@ -71,7 +71,7 @@ public class MareConfig : IMareConfiguration
     public bool ShowSyncConflictNotifications { get; set; } = true;
     public bool ShowAnalysisOnCompactUi {  get; set; } = true;
     public bool ShowAnalysisCompactUiBottom { get; set; } = false;
-    public bool ShowAnalysisCompactUiColor {  get; set; } = true;
-    public bool ShowCompactStats { get; set; } = false;
-    public bool ShowPlayerSyncName { get; set; } = false;
+    public bool ShowAnalysisCompactUiColor {  get; set; } = false;
+    public bool ShowCompactStats { get; set; } = true;
+    public bool ShowPlayerSyncName { get; set; } = true;
 }
