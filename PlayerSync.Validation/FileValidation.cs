3ecf8ee22760eb9a0415c5c29336b444edfab005
UniversalConquistador <universalconquistador@gmail.com>
2026-04-03 14:29:05 -0700

    Finishes initial implementation of client val.

--- PlayerSync.Validation/FileValidation.cs
+++ PlayerSync.Validation/FileValidation.cs
@@ -1,7 +1,8 @@
-﻿using PlayerSync.Validation.Tmb;
+﻿using Dalamud.Plugin;
+using Lumina.Excel;
+using PlayerSync.Validation.Tmb;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -9,26 +10,31 @@ namespace PlayerSync.Validation;
 
 public static class FileValidation
 {
+    public static void Initialize(IDalamudPluginInterface? pluginInterface)
+    {
+        if (pluginInterface != null)
+        {
+            pluginInterface.Inject(new VfxEditor.Dalamud());
+        }
+    }
+
     /// <summary>
-    /// Checks whether the given file fails validation according to any validation rules.
+    /// Checks the given file for any validation messages.
     /// </summary>
     /// <param name="fileData">The contents of the file to check.</param>
     /// <param name="extension">The extension of the file, as a testing hint. Pass a blank string if unknown.</param>
     /// <param name="validatePath">A callback used to determine whether the given game path exists.</param>
-    /// <param name="failure">The first validation failure that was found, if any.</param>
-    /// <returns>True if the file failed validation, or false if there were no failures.</returns>
-    public static bool IsInvalidFile(byte[] fileData, string extension, Func<string, bool>? validatePath, [NotNullWhen(true)] out ValidationFailure? failure)
+    /// <returns>The validation messages.</returns>
+    public static IEnumerable<ValidationMessage> ValidateFile(ExcelModule excelModule, byte[] fileData, string extension, Func<string, bool>? validatePath)
     {
         if (extension.Equals(".tmb", StringComparison.OrdinalIgnoreCase)
-            || (fileData.Length >= sizeof(uint) && MemoryMarshal.Read<uint>(fileData) == TmbValidation.Magic))
+            || (fileData.Length >= sizeof(uint) && MemoryMarshal.Read<uint>(fileData) == TmbValidation.TmbMagic))
         {
-            if (TmbValidation.IsInvalidFile(fileData, validatePath, out failure))
-            {
-                return true;
-            }
+            return TmbValidation.ValidateTmbFile(excelModule, fileData, validatePath);
         }
-
-        failure = null;
-        return false;
+        else
+        {
+            return Enumerable.Empty<ValidationMessage>();
+        }
     }
 }
--- PlayerSync.ValidationTest/ValidationTests.cs
+++ PlayerSync.ValidationTest/ValidationTests.cs
@@ -1,16 +1,30 @@
 ﻿using PlayerSync.Validation;
+using PlayerSync.Validation.Tmb;
 
 namespace PlayerSync.ValidationTest;
 
 [TestClass]
 public sealed class ValidationTests
 {
-    public record class ValidationTest(string TestFilePath, ValidationFailure? ExpectedFailure);
+    public record class ValidationTest(string TestFilePath, ValidationMessage? ExpectedFailure);
 
     private static readonly ValidationTest[] Tests =
     {
-        new("TMB001_a364d4e229a34968950610cd550e59a97cf2dfeb.tmb", Validation.Tmb.TmbValidation.TMB002),
-        new("TMB001_Valid_CheerJumpRed.tmb", null),
+        new("TMB002A_Invalid_human_sp233.tmb", TmbValidation.TMB002A),
+        new("TMB002B_Invalid_human_sp233.tmb", TmbValidation.TMB002B),
+        new("TMB002_Valid_human_sp233.tmb", null),
+
+        new("TMB010A_Invalid_CheerJumpRed.tmb", TmbValidation.TMB010A),
+        new("TMB010B_Invalid_CheerJumpRed.tmb", TmbValidation.TMB010B),
+        new("TMB010_Valid_CheerJumpRed.tmb", null),
+
+        new("TMB012A_a364d4e229a34968950610cd550e59a97cf2dfeb.tmb", TmbValidation.TMB012A),
+        new("TMB012B_a364d4e229a34968950610cd550e59a97cf2dfeb.tmb", TmbValidation.TMB012B),
+        new("TMB012_Valid_human_sp233.tmb", null),
+
+        new("TMB063A_Invalid_mon_sp001.tmb", TmbValidation.TMB063A),
+        new("TMB063B_Invalid_mon_sp001.tmb", TmbValidation.TMB063B),
+        new("TMB063_Valid_mon_sp001.tmb", null),
     };
 
     public static IEnumerable<object[]> GetTestData()
@@ -26,14 +40,21 @@ public sealed class ValidationTests
     public void TestValidation(ValidationTest test)
     {
         // For these tests we only check paths against the raw game data
-        // This would report a valid TMB that references a penumbra-supplied file as invalid, which is correct
+        // This would report a valid TMB that references a penumbra-supplied file as invalid, which is correct for these tests
         const string gameDataPath = "D:\\Program Files\\FINAL FANTASY XIV - A Realm Reborn\\game\\sqpack\\";
         var gameData = new Lumina.GameData(gameDataPath);
 
         byte[] fileData = File.ReadAllBytes(Path.Combine("TestFiles", test.TestFilePath));
-        var result = FileValidation.IsInvalidFile(fileData, Path.GetExtension(test.TestFilePath), path => gameData.FileExists(path), out var failure);
+        var result = FileValidation.ValidateFile(gameData.Excel, fileData, Path.GetExtension(test.TestFilePath), path => path.Contains('/') && gameData.FileExists(path));
 
-        Assert.AreSame(test.ExpectedFailure, failure);
-        Assert.AreEqual(test.ExpectedFailure != null, result);
+        Console.WriteLine($"{test.TestFilePath}: {string.Join(", ", result.Select(message => $"[{message.ID}]: {message.Title}"))}");
+        if (test.ExpectedFailure != null)
+        {
+            Assert.Contains(test.ExpectedFailure, result);
+        }
+        else
+        {
+            Assert.IsEmpty(result.Where(message => message.Level == Validation.MessageLevel.Crash));
+        }
     }
 }
